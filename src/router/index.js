70bd58fef9f56a8c1a2e5b9bb0ecd9e3a3cb46b3
zeta <1750596910@qq.com>
2020-05-03 14:15:02 +0800

    初始化router

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,37 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-  const routes = [
+//按需加载组件
+const Home = () => import("@/views/home/Home.vue");
+const Category = () => import("@/views/category/Category.vue");
+const Recommend = () => import("@/views/recommend/Recommend.vue");
+const Message = () => import("@/views/message/Message.vue");
+const User = () => import("@/views/user/User.vue");
+const Dashboard = () => import("@/views/dashboard/Dashboard.vue");
+
+const routes = [
+  { path: "/", redirect: "/dashboard" },
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/dashboard",
+    name: "dashboard",
+    redirect: "dashboard/home",
+    component: Dashboard,
+    children: [
+      { path: "home", name: "home", component: Home },
+      { path: "category", name: "category", component: Category },
+      { path: "recommend", name: "recommend", component: Recommend },
+      { path: "message", name: "message", component: Message },
+      { path: "user", name: "user", component: User },
+    ],
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
